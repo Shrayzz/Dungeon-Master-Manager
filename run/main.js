022bbd882a262afd0f1b0c90384767fbe106dd89
Shrayzz <103132558+Shrayzz@users.noreply.github.com>
2024-05-22 16:57:36 +0200

    ➕ Added First Structure
    🛠️ Updated "main.js"
    ➕ Added "base.css"
    ➕ Added "index.html"

---
+++ run/main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const fs = require('fs')
+
+const createMainWindow = () => {
+    const mainWindow = new BrowserWindow({
+        width: 1000,
+        height: 600,
+        minWidth: 640,
+        minHeight: 480,
+        frame: false,
+        icon: path.join(__dirname, '../Dungeon-Master-Manager.ico'),
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+        autoHideMenuBar: true,
+        titleBarStyle: "hidden",
+        titleBarOverlay: {
+            height: 40,
+            color: "#00000000",
+            symbolColor: "#ffffff",
+        },
+    })
+
+    mainWindow.loadFile('src/html/index.html')
+}
+
+app.on('ready', () => {
+    createMainWindow()
+})
