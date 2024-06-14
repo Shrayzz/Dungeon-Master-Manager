b7fb8033898dd4ce9d18614586ded35cfcd6bb5c
Shrayzz <103132558+Shrayzz@users.noreply.github.com>
2024-06-14 19:09:11 +0200

    ➕ Added missions location

--- Dungeon-Master-Manager/view/Map.xaml.cs
+++ Dungeon-Master-Manager/view/Map.xaml.cs
@@ -24,54 +24,5 @@ namespace Dungeon_Master_Manager.view
         {
             InitializeComponent();
         }
-        private void GeneratePoints()
-        {
-            // Coordonnées des points verts (extraites du script Python)
-            var coordinates = new (double, double)[]
-            {
-                (50, 710), (691, 704), (184, 644), (600, 608), (354, 600),
-                (164, 543), (294, 513), (614, 506), (469, 492), (67, 450),
-                (386, 428), (176, 393), (328, 376), (495, 363), (236, 342),
-                (605, 311), (336, 255), (221, 233), (626, 226), (538, 194),
-                (406, 155), (100, 91), (671, 38)
-            };
-
-            for (int i = 0; i < coordinates.Length; i++)
-            {
-                var coord = coordinates[i];
-                AddPoint(i, coord.Item1, coord.Item2);
-            }
-        }
-
-        private void AddPoint(int index, double x, double y)
-        {
-            Button pointButton = new Button
-            {
-                Width = 10,
-                Height = 10,
-                Background = Brushes.Red,
-                Tag = index // Attribuer l'index au Tag pour référence
-            };
-
-            // Ajustement de la position pour tenir compte des dimensions de l'image
-            double adjustedX = x * 300 / 1024;
-            double adjustedY = y * 300 / 1024;
-
-            Canvas.SetLeft(pointButton, adjustedX);
-            Canvas.SetTop(pointButton, adjustedY);
-
-            pointButton.Click += PointButton_Click;
-
-            MapCanvas.Children.Add(pointButton);
-        }
-
-        private void PointButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (sender is Button button)
-            {
-                int index = (int)button.Tag;
-                MessageBox.Show($"Point index: {index}");
-            }
-        }
     }
 }
