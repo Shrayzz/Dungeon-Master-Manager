8476473083d517d582a01bad2af5be0ebfedd4b1
Shrayzz <103132558+Shrayzz@users.noreply.github.com>
2024-06-14 19:28:03 +0200

    ➕ Added missions infos base structure

--- Dungeon-Master-Manager/view/Map.xaml.cs
+++ Dungeon-Master-Manager/view/Map.xaml.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Dungeon_Master_Manager.model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -23,6 +26,30 @@ namespace Dungeon_Master_Manager.view
         public Map()
         {
             InitializeComponent();
+            LoadButtons();
+        }
+        private void LoadButtons()
+        {
+            string jsonString = File.ReadAllText("../assets/missions.json");
+            var missions = JsonSerializer.Deserialize<Dictionary<string, Mission>>(jsonString);
+
+            int index = 1;
+            // Ajouter la récupération des buttons concernant les missions
+            // Syncroniser les index par rapport aux JSON
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button && button.Tag is Mission mission)
+            {
+                // Insérer les infos dans les boites de texte
+            }
+        }
+        public class Mission
+        {
+            public string? Name { get; set; }
+            public string? Description { get; set; }
+            public string? Rewards { get; set; }
         }
     }
 }
